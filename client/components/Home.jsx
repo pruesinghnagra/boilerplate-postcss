1976de9288d19e3bb418a9b32963f9f8531253f7
Prue Singh <pruesinghnagra@gmail.com>
2021-05-07 15:11:07 +1200

    Added custom and default tailwind classes

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 
 function Home () {
     return (
-        <main class="mt-12 lg:mt-32">
-            <section class="container mx-auto px-6">
-                <h1>Hello</h1>
+        <main className="mt-12 lg:mt-32">
+            <section className="container mx-auto px-6">
+                <h1 className='text-puce'>Hello</h1>
             </section>
+            <div class="flex">
+                <div class="flex-1">
+                    <p>ewewew</p>  
+                </div>
+                <div class="flex-none">
+                    <p>eweewe</p>
+                </div>
+                <div class="flex-1">
+                    <p>ewewewe</p>
+                </div>
+            </div>
         </main>
     )
 }
